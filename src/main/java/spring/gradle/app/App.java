bf5befcdd470dbf4186cc1719a18c6a5693d85ed
vamandeshmukh <vamandeshmukh@gmail.com>
2021-03-31 11:46:57 +0530

    Commit
    Commit

--- src/main/java/spring/gradle/app/App.java
+++ src/main/java/spring/gradle/app/App.java
@@ -1,11 +1,18 @@
 package spring.gradle.app;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
 	public static void main(String[] args) {
+
 		System.out.println("Start");
 
-		Employee emp = new Employee();
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+
+		Employee emp = context.getBean(Employee.class);
+
 		emp.printName();
 
 	}
