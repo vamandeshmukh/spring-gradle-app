afe4130c095dd84126ff16eee01ab4840edfc010
vamandeshmukh <vamandeshmukh@gmail.com>
2021-03-31 11:36:11 +0530

    Commit
    Commit

---
+++ src/main/java/spring/gradle/app/App.java
@@ -0,0 +1,13 @@
+package spring.gradle.app;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+
+		Employee emp = new Employee();
+		emp.printName();
+
+	}
+
+}
---
+++ src/main/java/spring/gradle/app/Employee.java
@@ -0,0 +1,11 @@
+package spring.gradle.app;
+
+public class Employee {
+
+	String name;
+
+	public void printName() {
+		System.out.println("Employee's name " + this.name);
+	}
+
+}
